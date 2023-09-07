115f505d5cd6dcbad9ae88e67cac271975f82db7
Collins <collinskiarie13@outlook.com>
2023-09-07 22:10:58 +0300

    Added a functionality to delete an activity

--- app/trip_management.py
+++ app/trip_management.py
@@ -1,6 +1,9 @@
 from user import User 
 from sqlalchemy.orm import sessionmaker
-from database import engine
+from database import engine, Session
+from activity import Activity
+from destination import Destination
+from budget import update_budget, add_activity_cost_to_budget
 
 Session = sessionmaker(bind=engine)
 
@@ -20,3 +23,30 @@ def list_user_trips(user_id):
                 print(f"Trip ID: {trip.id}, Trip Name: {trip.trip_name}")
 
     session.close()
+
+def delete_activity(activity_id):
+    session = Session()
+
+    # Query the database to find the activity with the specified activity_id
+    activity = session.query(Activity).filter_by(id=activity_id).first()
+
+    if activity:
+        # Get the cost of the activity
+        cost = activity.cost
+
+        # Get the destination_id to pass to update_budget
+        destination_id = activity.destination_id
+
+        # Delete the activity
+        session.delete(activity)
+        session.commit()
+        print(f"Activity '{activity.name}' deleted.")
+
+        # Add the cost of the deleted activity back to the budget
+        add_activity_cost_to_budget(destination_id, cost)
+        print(f"Cost ({cost}) added back to the budget.")
+    else:
+        print("Activity not found.")
+
+    session.close()
+
