747ceec703f076231b10ae7d1bf753f1be13a15c
Collins <collinskiarie13@outlook.com>
2023-09-07 22:09:42 +0300

    Added a functionality thats add back the cost of a deleted activity back to the trip budget

--- app/budget.py
+++ app/budget.py
@@ -22,3 +22,21 @@ def update_budget(destination_id, cost):
         print("Destination not found.")
 
     session.close()
+
+def add_activity_cost_to_budget(destination_id, cost):
+    session = Session()
+
+    # Check if the destination exists
+    destination = session.query(Destination).filter_by(id=destination_id).first()
+
+    if destination:
+        trip = destination.trip
+
+        # Add the cost of the deleted activity back to the trip budget
+        trip.budget += cost
+        session.commit()
+        print("successful.")
+    else:
+        print("Destination not found.")
+
+    session.close()
